15bd1402ca63465c002e2ea0808f0eec9e976259
Kovalev Dmitriy <kacevnik@yandex.ru>
2022-05-20 23:53:08 +0300

    Added fonts and header

--- assets/src/components/header/header.js
+++ assets/src/components/header/header.js
@@ -1,6 +1,9 @@
 import gsap from 'gsap';
 import InputMask from 'inputmask';
 
-window.onload = function() {
-
-}
+jQuery(document).ready(function( $ ) {
+    $(document).on('click', '.hamburger', function(){
+        $(this).toggleClass('is-active');
+        $('.header-nav ul').toggleClass('__show');
+    });
+});
