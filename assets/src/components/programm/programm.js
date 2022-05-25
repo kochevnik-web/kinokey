8d33fca665f9f8aa49c168542d5e208b87ebb288
Kovalev Dmitriy <kacevnik@yandex.ru>
2022-05-26 02:01:21 +0300

    Finish programm page

---
+++ assets/src/components/programm/programm.js
@@ -0,0 +1,43 @@
+import Swiper from 'swiper/bundle';
+import tooltip from 'bootstrap';
+
+jQuery(document).ready(function( $ ) {
+     new Swiper('.programm-swiper', {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        loopFillGroupWithBlank: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+         breakpoints: {
+             1200: {
+                 slidesPerView: 7,
+                 slidesPerGroup: 7
+             },
+             // when window width is >= 480px
+             992: {
+                 slidesPerView: 5,
+                 slidesPerGroup: 5,
+             },
+             // when window width is >= 640px
+             768: {
+                 slidesPerView: 4,
+                 slidesPerGroup: 4,
+             }
+         }
+    });
+
+    $('body').tooltip({
+        selector: '[data-toggle="tooltip"]',
+        html: true,
+        trigger: 'click hover',
+        delay: {
+            show: 50,
+            hide: 400
+        }
+    })
+
+});
